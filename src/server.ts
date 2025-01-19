757f79fe2422d364e99fc03c12adc828ea034d77
Sidney Santos <sidney.sasantos@outlook.com>
2025-01-19 16:29:24 -0300

    Chore: Ajuste na Porta 3333.

--- src/server.ts
+++ src/server.ts
@@ -2,5 +2,5 @@ import { app } from "./app";
 import { env } from "./env";
 
 app.listen({ port: env.PORT }).then(() => {
-  console.log("Server is running on port 3000");
+  console.log("Server is running on port 3333");
 });
